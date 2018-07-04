2bab55a3bfd021b62ddc8817c199731fcc6aad5c
Mogan Xumerle <moganrz@gmail.com>
2018-07-03 23:01:27 -0300

    alura django - listando todos os perfis

--- perfis/views.py
+++ perfis/views.py
@@ -4,7 +4,7 @@ from perfis.models import Perfil
 
 def index(request):
     #return HttpResponse("Bem Vindo ao Connectedin!")
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'perfis' : Perfil.objects.all()})
 
 def exibir(request, perfil_id):
     #print("Entrou e deu certo o id de perfil {}".format(perfil_id))
